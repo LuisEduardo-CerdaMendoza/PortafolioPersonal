cc9bacb011c81dca4003e2006d7ea0b835dc7bd0
Luis Eduardo Cerda Mendoza <154579730+LuisEduardo-CerdaMendoza@users.noreply.github.com>
2024-06-11 13:45:56 -0600

    Update Index.jsx

--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -27,6 +27,9 @@ const Index = () => {
             <a href="mailto:luiseduardo.cerdamendoza@outlook.com" target="_blank" rel="noopener noreferrer">
               <img src="https://img.shields.io/badge/Outlook-0078D4?style=for-the-badge&logo=microsoft-outlook&logoColor=white" alt="Outlook" />
             </a>
+            <a href="wa.me/5561446056" target="_blank" rel="noopener noreferrer">
+              <img src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" alt="WhatsApp" />
+            </a>
           </div>
         </div>
       </div>
